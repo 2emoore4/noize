44622f33b7471351f05a2d125288c9e0c98c870e
Evan Moore <2emoore4@gmail.com>
2014-03-04 17:24:13 -0500

    this thing will be a web drawing thing which uses pixi.js

---
+++ draw.js
@@ -0,0 +1,11 @@
+draw_polygon = function() {
+    var points = new Array();
+
+    var add_point = function(point) {
+        points[point.length] = point;
+    }
+}
+
+function noise() {
+    return (Math.random() - 0.5) * 3;
+}
